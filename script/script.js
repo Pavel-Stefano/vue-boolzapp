9138e5417a3c20e8ae8b1e34c03c9808b43ca6a9
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-05-04 12:11:11 +0200

    color

--- script/script.js
+++ script/script.js
@@ -9,16 +9,19 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -32,16 +35,19 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+                        display: false,
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -55,16 +61,19 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        display: false,
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -78,11 +87,13 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -96,11 +107,13 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -114,16 +127,19 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -137,11 +153,13 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Fa gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -155,16 +173,19 @@ const app = new Vue({
                 visible: 'true',
                 messages: [
                     {
+                        display: false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi! ',
                         status: 'sent'
                     },
                     {
+                        display: false,
                         date: '10/01/2020 15:51:00',
                         message: 'Ok!!',
                         status: 'received'
@@ -206,6 +227,23 @@ const app = new Vue({
                 this.contacts[this.currentIndex].messages.push(rispostaMessage);
             }, 2000)
 
+        },
+        // showMenu: function(activeContactIndex, i){
+        //     if (this.contacts[activeContactIndex].messages[i].display === false) {
+        //         this.contacts[activeContactIndex].messages.forEach((messaggio)=>{
+        //             messaggio.display = false
+        //         })
+        //     } 
+        //     this.contacts[activeContactIndex].messages[i].display = !this.contacts[activeContactIndex].messages[i].display
+        // },
+
+        dropDownMenu(currentIndex, index){
+            if(this.contacts[currentIndex].messagges.length > 0){
+                this.contacts[currentIndex].messagges.splice(index, 1)
+            }
+            else {
+                return
+            }
         }
     },
     computed: {
